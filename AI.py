882e73a5aefc027992ab1d3c49d26ccb240be2b8
700brains <myprogramminglive@gmail.com>
2017-04-11 20:46:40 +0100

    Create AI.py

---
+++ AI.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pyttsx
+import time
+ 
+ 
+ engine = init.pyttsx()
+# get a list of keywords for the Ai
+lis={'keywords here", "separated by commas" } 
+# print and say a greeting message
+ engine.say("welcome")
+ print "welcome" 
+ # user response 
+ Lis2 ={  } 
+really = input() 
+for word in Lis2:
+   if really is in word:
+      Esa = really
+      
